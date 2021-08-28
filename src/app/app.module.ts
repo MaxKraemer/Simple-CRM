f1bf99d4f2d60ee827b518cc1c1852593b2cc9f2
Max Kraemer <maxkraemer@Maxs-MacBook-Air.local>
2021-08-28 21:36:51 +0200

    user dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    UserComponent
+    UserComponent,
+    DialogAddUserComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +35,12 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatSidenavModule,
     MatIconModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     
 
   
